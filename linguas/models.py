f99714f57f3dfedd2d2ce1ee74de9603d41f85f6
Miguel Gaiowski <miggaiowski@gmail.com>
2010-11-08 22:27:42 -0200

    mais algumas tabelas em models.py

--- linguas/models.py
+++ linguas/models.py
@@ -20,3 +20,25 @@ class Idioma(models.Model):
     nome = models.CharField(max_length=200)
     pag_wiki = models.URLField(max_length=200)
 
+class Local_do_Idioma(models.Model):
+    idioma = models.ForeignKey(Idioma)
+    pais = models.CharField(max_length=200)
+    regiao = models.CharField(max_length=200)
+
+class Keyword(models.Model):
+    texto = models.CharField(max_length=50, unique=True)
+    idioma = models.ForeignKey(Idioma)
+
+class Natureza(models.Model):
+    tipo = models.CharField(max_length=200)
+
+class Usuario(models.Model):
+    login = models.CharField(max_length=30)
+    nome = models.CharField(max_length=200)
+    email = models.EmailField()
+    senha = models.CharField(max_length=20)
+    tipo = models.CharField(max_length=30)
+    pais_sede = models.CharField(max_length=200)
+
+
+    
