7d2c237922a939918b8ab663585a552cb2c922de
Miguel Gaiowski <miggaiowski@gmail.com>
2010-11-12 12:30:34 -0200

    URL para lista de keywords funcionando

--- urls.py
+++ urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import *
+from django.views.generic import DetailView, ListView
+from linguas.models import Keyword
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -7,7 +9,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^trabalho/', include('trabalho.foo.urls')),
-    (r'^keyword/$', ListView.as_view(model=Usuario, template_name='linguas/keywords.html')),
+    (r'^keyword/$', ListView.as_view(model=Keyword, context_object_name='keyword_list', template_name='linguas/keywords.html')),
     # (r'^keywords/$', linguas.views.keywords),
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
