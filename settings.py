670f1959f94bb7008d72fb3ab97610160af91c3a
Miguel Gaiowski <miggaiowski@gmail.com>
2010-11-12 18:30:22 -0200

    Atualizei a versao do django e agora o css funciona

--- settings.py
+++ settings.py
@@ -1,13 +1,15 @@
 # Django settings for trabalho project.
 
+import os
+
+PATH_PWD = '/Users/miguelgaiowski/src/mc536/trabalho/'
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Miguel Gaiowski', 'miggaiowski@gmail.com'),
+    ('Miguel Gaiowski', 'miggaiowski@gmail.com'),
 )
-
-PATH_PWD = "/Users/miguelgaiowski/src/mc536/trabalho/"
 
 MANAGERS = ADMINS
 
@@ -46,7 +48,7 @@ USE_I18N = True
 USE_L10N = True
 
 # Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
+# Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
@@ -54,13 +56,43 @@ MEDIA_ROOT = ''
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = ''
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/static/"
+# STATICFILES_ROOT = ''
+
+# URL that handles the static files served from STATICFILES_ROOT.
+# Example: "http://static.lawrence.com/", "http://example.com/static/"
+# STATICFILES_URL = '/static/'
+
+# URL prefix for admin media -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+# ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# A list of locations of additional static files
+# STATICFILES_DIRS = ()
+
+# Configs do cara do IRC =)
+STATICFILES_URL = '/static/'
+STATICFILES_ROOT = os.path.join(os.path.dirname(__file__), 'static')
+STATICFILES_DIRS = (
+    STATICFILES_ROOT,
+)
+
+
 ADMIN_MEDIA_PREFIX = '/media/'
 
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '^u+75*i-jf=atbtdi38wzidu1ns2#c32i1h*zma!b_88!rjp9a'
+SECRET_KEY = 't9b-zf5%q_3l5lf135u*uq@8izo5(hti80-1fe^x7jaz-hxday'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -68,15 +100,6 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
 )
-
-# TEMPLATE_CONTEXT_PROCESSORS = (
-#     'django.core.context_processors.debug',
-#     'django.core.context_processors.i18n',
-#     'django.core.context_processors.media',
-#     'django.contrib.auth.context_processors.auth',
-#     'django.contrib.messages.context_processors.messages',
-#     'django.contrib.staticfiles.context_processors.staticfiles',
-# )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
@@ -89,14 +112,11 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'trabalho.urls'
 
 TEMPLATE_DIRS = (
+    PATH_PWD + 'templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    PATH_PWD + 'templates',
 )
-
-STATICFILES_ROOT = PATH_PWD + 'static'
-STATICFILES_URL = '/static/'
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -111,3 +131,26 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request':{
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
